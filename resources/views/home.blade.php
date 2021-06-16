43b178e5c677b20c1390051caab8e1aae64e5890
Fabio Jose Moretti <fabioj.moretti@gmail.com>
2021-06-16 16:03:21 -0300

    Hotfix home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,7 @@
 @section('title') FFLights @endsection
 
 @section('content')
-<<<<<<< HEAD
     
-=======
-    Agora vai sim!
->>>>>>> ca2c686e47f21fe9505535ab255fcad55441f51a
 @endsection
 
 @section('footer')
